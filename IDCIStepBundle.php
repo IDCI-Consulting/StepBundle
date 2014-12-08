f15c5b794476320e1b6ec50ff51e7a359971c187
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2014-12-08 17:18:01 +0100

    Add the bundle entry point class

---
+++ IDCIStepBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ *
+ */
+
+namespace IDCI\Bundle\StepBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IDCIStepBundle extends Bundle
+{
+}
