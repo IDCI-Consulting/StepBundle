7bbf22ed2e952c12f5b1c533f88564b2294910ae
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2014-12-08 19:11:40 +0100

    Add default dependecy injection files

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ *
+ */
+
+namespace IDCI\Bundle\StepBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('idci_step');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+        $rootNode
+        ;
+
+        return $treeBuilder;
+    }
+}
