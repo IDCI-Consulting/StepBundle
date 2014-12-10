38bb0130c65322694cf2fcce28bf4523e3b17d91
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2014-12-10 18:39:17 +0100

    Rework

---
+++ Step/StepRegistryInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ */
+ 
+namespace IDCI\Bundle\StepBundle\Registry;
+
+use IDCI\Bundle\StepBundle\Type\StepTypeInterface;
+
+interface StepRegistryInterface
+{
+    /**
+     * Sets a step type identify by a name
+     *
+     * @param string            $name   The step name
+     * @param StepTypeInterface $step   The step
+     */
+    public function setType($name, StepTypeInterface $step);
+
+    /**
+     * Returns a step type by name.
+     *
+     * @param string $name The name of the type
+     *
+     * @return StepTypeInterface The type
+     *
+     * @throws Exception\UnexpectedTypeException  if the passed name is not a string
+     * @throws Exception\InvalidArgumentException if the type can not be retrieved
+     */
+    public function getType($name);
+
+    /**
+     * Returns whether the given step type is supported.
+     *
+     * @param string $name The name of the type
+     *
+     * @return bool Whether the type is supported
+     */
+    public function hasType($name);
+}
