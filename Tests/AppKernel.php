19a8bfa4760ce853f4b7ea74d3054c3296d4e916
Gnucki <tprelot@gmail.com>
2014-12-15 18:39:39 +0100

    Allow application tests

---
+++ Tests/AppKernel.php
@@ -0,0 +1,25 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array();
+
+        if (in_array($this->getEnvironment(), array('test'))) {
+            $bundles[] = new Symfony\Bundle\FrameworkBundle\FrameworkBundle();
+            $bundles[] = new JMS\SerializerBundle\JMSSerializerBundle();
+            $bundles[] = new IDCI\Bundle\StepBundle\IDCIStepBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config.yml');
+    }
+}
--- Tests/Flow/FlowProviderTest.php
+++ Tests/Flow/FlowProviderTest.php
@@ -2,11 +2,25 @@
 
 namespace IDCI\Bundle\StepBundle\Flow;
 
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
 /**
  * @author Thomas Prelot <thomas.prelot@tessi.fr>
  */
-class FlowProviderTest extends \PHPUnit_Framework_TestCase
+class FlowProviderTest extends WebTestCase
 {
+    private $container;
+
+    protected function setUp()
+    {
+        require_once __DIR__.'/../AppKernel.php';
+
+        $kernel = new \AppKernel('test', true);
+        $kernel->boot();
+        $kernel->loadClassCache();
+        $this->container = $kernel->getContainer();
+    }
+
     public function getData()
     {
         $data = array();
@@ -25,6 +39,7 @@ class FlowProviderTest extends \PHPUnit_Framework_TestCase
      */
     public function testProcess($plop, $plip)
     {
+        $flowProvider = $this->container->get('idci_step.flow.provider');
         $this->assertEquals(1, $plop);
     }
 }
