a8d5f269a3ed61d8eb29c2c72d2640462162eb85
Gnucki <tprelot@gmail.com>
2015-01-07 17:58:00 +0100

    Add flow events

--- Step/Type/AbstractStepType.php
+++ Step/Type/AbstractStepType.php
@@ -7,6 +7,7 @@
 
 namespace IDCI\Bundle\StepBundle\Step\Type;
 
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use IDCI\Bundle\StepBundle\Step\StepInterface;
 use IDCI\Bundle\StepBundle\Map\MapInterface;
@@ -29,7 +30,7 @@ abstract class AbstractStepType implements StepTypeInterface
                 'listeners'        => array(
                     'before' => array(),
                     'after'  => array()
-                )
+                ),
             ))
             ->setAllowedTypes(array(
                 'title'            => array('null', 'string'),
@@ -37,6 +38,56 @@ abstract class AbstractStepType implements StepTypeInterface
                 'is_first'         => array('bool'),
                 'previous_options' => array('array'),
                 'listeners'        => array('array'),
+            ))
+            ->setAllowedValues(array(
+                'listeners' => function ($value) {
+                    if (!is_array($value)) {
+                        return false;
+                    }
+                    if (count($value) >= 3) {
+                        return false;
+                    }
+
+                    foreach ($value as $key => $val) {
+                        if (!in_array($key, array('before', 'after'))) {
+                            return false;
+                        }
+
+                        if (!is_array($val)) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                },
+            ))
+            ->setNormalizers(array(
+                'listeners' => function (Options $options, $value) {
+                    $formattedValue = array(
+                        'before' => array(),
+                        'after' => array()
+                    );
+
+                    foreach ($value as $time => $listeners) {
+                        foreach ($value[$time] as $key => $val) {
+                            if (is_array($val)) {
+                                // Handle case ['alias' => array(...)].
+                                if (is_string($key)) {
+                                    $val['alias'] = $key;
+                                    $formattedValue[$time][] = $val;
+                                // Handle case [0 => array('alias' => '', ...)].
+                                } else {
+                                    $formattedValue[$time][] = $val;
+                                }
+                            // Handle case [0 => 'alias'].
+                            } else {
+                                $formattedValue[$time][] = array('alias' => $val);
+                            }
+                        }
+                    }
+
+                    return $formattedValue;
+                },
             ))
         ;
     }
