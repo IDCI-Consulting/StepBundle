a45be779a9fb51e6f1147cb7f7a822dfd0720bcf
Gnucki <tprelot@gmail.com>
2015-01-23 16:43:23 +0100

    Add configuration fetcher configuration

---
+++ Configuration/Fetcher/ConfigurationFetcher.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Thomas Prelot <tprelot@gmail.com>
+ * @license: MIT
+ */
+
+namespace IDCI\Bundle\StepBundle\Configuration\Fetcher;
+
+class ConfigurationFetcher extends AbstractConfigurationFetcher
+{
+    protected $raw;
+
+    /**
+     * Constructor.
+     *
+     * @param array $raw
+     */
+    public function __construct(array $raw)
+    {
+        $this->raw = $raw;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function doFetch(array $parameters = array())
+    {
+        return $this->raw;
+    }
+}
--- DependencyInjection/Compiler/ConfigurationFetcherCompilerPass.php
+++ DependencyInjection/Compiler/ConfigurationFetcherCompilerPass.php
@@ -10,6 +10,7 @@ namespace IDCI\Bundle\StepBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 
 class ConfigurationFetcherCompilerPass implements CompilerPassInterface
 {
@@ -23,6 +24,22 @@ class ConfigurationFetcherCompilerPass implements CompilerPassInterface
         }
 
         $registryDefinition = $container->getDefinition('idci_step.configuration.fetcher_registry');
+
+        $configurations = $container->getParameter('idci_step.config.maps');
+        foreach ($configurations as $name => $configuration) {
+            $fetcherDefinition = new DefinitionDecorator('idci_step.configuration.fetcher.configuration');
+            $fetcherServiceId = sprintf('idci_step.configuration.fetcher.configuration.%s', $name);
+
+            $fetcherDefinition->replaceArgument(0, $configuration);
+
+            $container->setDefinition($fetcherServiceId, $fetcherDefinition);
+
+            $registryDefinition->addMethodCall(
+                'setFetcher',
+                array($name, new Reference($fetcherServiceId))
+            );
+        }
+
         foreach ($container->findTaggedServiceIds('idci_step.configuration.fetcher') as $id => $tags) {
             foreach ($tags as $attributes) {
                 $alias = isset($attributes['alias'])
