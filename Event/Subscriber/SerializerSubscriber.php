d8bc34b1fd50352dec2c0935cfaf73159bdd9ce8
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2015-03-04 11:09:29 +0100

    Refactor serializer subscriber, remove the serializer handler

--- Event/Subscriber/SerializerSubscriber.php
+++ Event/Subscriber/SerializerSubscriber.php
@@ -6,6 +6,7 @@ use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
 use JMS\Serializer\EventDispatcher\ObjectEvent;
 use JMS\Serializer\GraphNavigator;
 use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\DeserializationContext;
 use IDCI\Bundle\StepBundle\Serializer\SerializationMapper;
 use IDCI\Bundle\StepBundle\Flow\FlowData;
 
@@ -38,6 +39,7 @@ class SerializerSubscriber implements EventSubscriberInterface
     {
         return array(
             array(
+                'class'     => 'IDCI\Bundle\StepBundle\Flow\FlowData',
                 'event'     => Events::POST_DESERIALIZE,
                 'direction' => GraphNavigator::DIRECTION_DESERIALIZATION,
                 'method'    => 'onPostDeserialize',
@@ -53,28 +55,46 @@ class SerializerSubscriber implements EventSubscriberInterface
         $object  = $event->getObject();
         $context = $event->getContext();
 
-        if ($object instanceof \IDCI\Bundle\StepBundle\Flow\FlowData) {
-            foreach ($object->getFormTypeMapping() as $step => $fields) {
-                foreach ($fields as $field => $formType) {
-                    $fieldType = $this->mapper->map('form_types', $formType);
-                    if (null !== $fieldType) {
-                        if ($object->hasStepData($step, FlowData::TYPE_REMINDED)) {
-                            $data = $object->getStepData($step, FlowData::TYPE_REMINDED);
-                            if (isset($data[$field])) {
-                                $typeParser = new \JMS\Serializer\TypeParser();
-                                $data[$field] = $context
-                                    ->getNavigator()
-                                    ->accept(
-                                        $context->getVisitor()->prepare(json_encode($data[$field])),
-                                        $typeParser->parse($fieldType),
-                                        $context
-                                    )
-                                ;
-                                $object->setStepData($step, $data, array(), FlowData::TYPE_REMINDED);
-                            }
-                        }
-                    }
+        foreach ($object->getFormTypeMapping() as $step => $fields) {
+            foreach ($fields as $field => $formType) {
+                $fieldType = $this->mapper->map('form_types', $formType);
+                if (null !== $fieldType) {
+                    $this->transformData($object, $step, $field, $fieldType, $context);
+                    $this->transformData($object, $step, $field, $fieldType, $context, FlowData::TYPE_REMINDED);
                 }
+            }
+        }
+    }
+
+    /**
+     * Transform data
+     *
+     * @param FlowData               $object   The flow data to transform.
+     * @param string                 $step     The step name.
+     * @param string                 $field    The field step name.
+     * @param string                 $type     To transform into this type.
+     * @param DeserializationContext $context  The deserialization context.
+     * @param string                 $dataType 
+     */
+    protected function transformData(
+        FlowData & $object,
+        $step,
+        $field,
+        $type,
+        DeserializationContext $context,
+        $dataType = null
+    )
+    {
+        if ($object->hasStepData($step, $dataType)) {
+            $data = $object->getStepData($step, $dataType);
+            if (isset($data[$field])) {
+                $typeParser = new \JMS\Serializer\TypeParser();
+                $data[$field] = $context->getNavigator()->accept(
+                    $data[$field],
+                    $typeParser->parse($type),
+                    $context
+                );
+                $object->setStepData($step, $data, array(), $dataType);
             }
         }
     }
