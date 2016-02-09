fbe36a8c22fc3978ca4bb7776a33436166f26753
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2016-02-09 17:32:14 +0100

    Add new abstract class

---
+++ Step/Type/Form/AbstractStepFormType.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ */
+
+namespace IDCI\Bundle\StepBundle\Step\Type\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+abstract class AbstractStepFormType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_merge($view->vars, array(
+            'display_title' => $options['display_title'],
+            'attr'          => $options['attr'],
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setDefaults(array(
+                'display_title' => true,
+                'attr'          => array(),
+            ))
+            ->setAllowedTypes(array(
+                'display_title' => array('bool'),
+                'attr'          => array('array'),
+            ))
+        ;
+    }
+}
