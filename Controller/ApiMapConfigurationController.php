31e2fa9d2d80a8c4cc29e020684695334c03e933
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-04-19 17:21:11 +0200

    Expose the options of the map with a new api controller

---
+++ Controller/ApiMapConfigurationController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace IDCI\Bundle\StepBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Controller\Annotations as Annotations;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Api MapConfiguration Controller
+ */
+class ApiMapConfigurationController extends FOSRestController
+{
+    /**
+     * [GET] /map-configuration
+     *
+     * Retrieve the map configuration
+     *
+     * @Get("/map-configuration.{_format}")
+     *
+     * @param string $_format
+     *
+     * @return Response
+     */
+    public function getMapConfigurationAction($_format)
+    {
+        $view = View::create()->setFormat($_format);
+        $configuration = array(
+            'extra_form_options' => array(
+                'display_step_in_url' => array(
+                    'extra_form_type' => 'checkbox',
+                    'options' => array(
+                        'required' => false,
+                        'data'     => false
+                    )
+                ),
+                'final_destination' => array(
+                    'extra_form_type' => 'text',
+                    'options' => array(
+                        'required' => false
+                    )
+                ),
+                'browsing' => array(
+                    'extra_form_type' => 'choice',
+                    'options' => array(
+                        'choices'  => array(
+                            'linear' => 'linear',
+                            'free' => 'free'
+                        ),
+                        'data' => 'linear',
+                        'required' => false
+                    )
+                ),
+                'form_action' => array(
+                    'extra_form_type' => 'text',
+                    'options' => array(
+                        'required' => false
+                    )
+                ),
+                'first_step_name' => array(
+                    'extra_form_type' => 'text',
+                    'options' => array(
+                        'required' => false
+                    )
+                ),
+            )
+        );
+
+        $view->setData($configuration);
+
+        return $this->handleView($view);
+    }
+}
