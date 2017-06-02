d1ae1cbb63147fae8c6f0181f813eb530996a3ae
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-06-02 09:30:10 +0200

    Remove setAllowedTypes as the option is normalized after

--- Step/Event/Action/AbstractStepEventAction.php
+++ Step/Event/Action/AbstractStepEventAction.php
@@ -29,9 +29,6 @@ abstract class AbstractStepEventAction implements StepEventActionInterface
                     return (bool)$value;
                 },
             ))
-            ->setAllowedTypes(array(
-                'logical_expression' => array('bool'),
-            ))
         ;
 
         $this->setDefaultParameters($resolver);
